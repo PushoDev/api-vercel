cafda8da619e3d5061340de4340b029f7c9b8a8f
PushoDev <bethocubans1990@gmail.com>
2024-12-02 17:36:00 -0500

    Deploy in Vercel - Config vercel.json 1

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.get("/api", (req, res) => {
   res.send(`<h1 style="color:blue"> 
-		Inicio de la API Rest</h1>`);
+		Hola Mundo</h1>`);
 });
 app.listen(8080, () => {
   console.log("Server started at http://localhost:8080");
